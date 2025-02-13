9cc09a773b4d01a4485974ea6c3c62b69b8277a8
yusitk <kirillzelenovv@gmail.com>
2025-02-13 16:32:34 +0300

    Delete filesgp directory

--- filesgp/migrations/001_init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE files (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    filename TEXT NOT NULL,
-    filetype TEXT NOT NULL,
-    filesize BIGINT NOT NULL,
-    storage_url TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
